29574c5ec39b9cee30842f2eb763ed2ec7038f5e
Bpost129 <Bpost129@yahoo.com>
2025-12-17 12:01:53 -0500

    refactor tasks router to render task index view, import tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 
+import { tasks } from '../data/task-data.js'
+
 const router = Router()
 
 // GET localhost:3000/tasks
 router.get('/', function(req, res) {
-  res.send('respond with a resource')
+  res.render('tasks/index', {
+    tasks: tasks
+  })
 })
 
 export { router }
