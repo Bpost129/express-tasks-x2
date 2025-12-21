3eabf401e211cba9e7989efc6ccf088295809b4c
Bpost129 <Bpost129@yahoo.com>
2025-12-20 21:35:18 -0500

    refactor task list to make tasks redirect

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,6 +7,7 @@ const router = Router()
 // GET localhost:3000/tasks
 router.get('/', tasksCtrl.index)
 router.get('/new', tasksCtrl.new)
+router.get('/:taskId', tasksCtrl.show)
 router.post('/', tasksCtrl.create)
 
 export { router }
